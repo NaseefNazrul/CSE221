34229de7bb3cd66b8b75006c8088f3656cc0b14c
Naseef Nazrul <naseefnazrul@gmail.com>
2026-08-04 17:34:47 +0600

    still C

--- Assignment/5/C/solution.java
+++ Assignment/5/C/solution.java
@@ -1,8 +1,12 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class solution {
@@ -16,6 +20,9 @@ public class solution {
         int end = Integer.parseInt(st.nextToken());
 
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= cities; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
 
         int [] u = new int[roads];
         int [] v = new int[roads];
@@ -37,6 +44,46 @@ public class solution {
             Collections.sort(graph.get(i));
         } // made the graph 
 
+        
+        boolean [] visited = new boolean[cities+1];
+        int [] parent = new int[cities+1];
+        Queue<Integer> queue = new LinkedList<>();
+        StringBuilder ans = new StringBuilder();
 
+        visited[start] = true;
+        queue.offer(start);
+        parent[start] = -1;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            ans.append(current).append(" ");
+
+            for (int neighbour : graph.get(current)) {
+                if (!visited[neighbour]) {
+                    parent[neighbour] = current;
+                    visited[neighbour] = true;
+                    queue.offer(neighbour);
+                }
+            }
+        } // BFS search
+
+
+        int temp = end;
+        Deque<Integer> stack = new ArrayDeque<>();
+        while (temp != start ) {
+            stack.push(temp);
+            temp = parent[temp];
+        } // So that the path gets reversed using a stack
+        stack.push(temp);
+
+        StringBuilder path = new StringBuilder();
+        int count = -1;
+        while (!stack.isEmpty()) {
+            path.append(stack.pop()).append(" ");
+            count++;
+        }
+        pw.println(count);
+        pw.println(path);
+        pw.close();
     }
 }
